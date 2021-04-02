74daedd30276fc08ea6037f279ca76e2f28da949
Smail-ssm <ismailmansouri571@gmail.com>
2021-04-02 22:54:44 +0100

    chatobject todo

--- app/src/main/java/com/xdev/deliverytn/localHelper.java
+++ app/src/main/java/com/xdev/deliverytn/localHelper.java
@@ -1,58 +1,54 @@
 package com.xdev.deliverytn;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.content.res.Resources;
+import android.os.Build;
 import android.preference.PreferenceManager;
 
 import java.util.Locale;
 
 public class localHelper {
-
-
     private static final String SELECTED_LANGUAGE = "Locale.Helper.Selected.Language";
 
-    public static void onCreate(Context context) {
-
-        String lang;
-        if (getLanguage(context).isEmpty()) {
-            lang = getPersistedData(context, Locale.getDefault().getLanguage());
-        } else {
-            lang = getLanguage(context);
-        }
-
-        setLocale(context, lang);
-    }
-
-    public static void onCreate(Context context, String defaultLanguage) {
-        String lang = getPersistedData(context, defaultLanguage);
-        setLocale(context, lang);
-    }
-
-    public static String getLanguage(Context context) {
-        return getPersistedData(context, Locale.getDefault().getLanguage());
-    }
-
-    public static void setLocale(Context context, String language) {
+    // the method is used to set the language at runtime
+    public static Context setLocale(Context context, String language) {
         persist(context, language);
-        updateResources(context, language);
-    }
 
-    private static String getPersistedData(Context context, String defaultLanguage) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getString(SELECTED_LANGUAGE, defaultLanguage);
+        // updating the language for devices above android nougat
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            return updateResources(context, language);
+        }
+        // for devices having lower version of android os
+        return updateResourcesLegacy(context, language);
     }
 
     private static void persist(Context context, String language) {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
         SharedPreferences.Editor editor = preferences.edit();
-
         editor.putString(SELECTED_LANGUAGE, language);
         editor.apply();
     }
 
-    private static void updateResources(Context context, String language) {
+    // the method is used update the language of application by creating
+    // object of inbuilt Locale class and passing language argument to it
+    @TargetApi(Build.VERSION_CODES.N)
+    private static Context updateResources(Context context, String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+
+        Configuration configuration = context.getResources().getConfiguration();
+        configuration.setLocale(locale);
+        configuration.setLayoutDirection(locale);
+
+        return context.createConfigurationContext(configuration);
+    }
+
+
+    @SuppressWarnings("deprecation")
+    private static Context updateResourcesLegacy(Context context, String language) {
         Locale locale = new Locale(language);
         Locale.setDefault(locale);
 
@@ -60,9 +56,12 @@ public class localHelper {
 
         Configuration configuration = resources.getConfiguration();
         configuration.locale = locale;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+            configuration.setLayoutDirection(locale);
+        }
 
         resources.updateConfiguration(configuration, resources.getDisplayMetrics());
 
-
+        return context;
     }
 }
